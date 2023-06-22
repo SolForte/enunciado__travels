4aa6432b75a7b08c625e3e8425553a4c2f867c0d
SolForte <mateusbrandao1234@gmail.com>
2023-06-21 21:11:44 -0300

    fix: removal of unnecesary code

--- src/controllers/passagers.controllers.js
+++ src/controllers/passagers.controllers.js
@@ -2,10 +2,8 @@ import httpStatus from "http-status";
 import passagersRepositories from "../repositories/passagers.repositories.js";
 
 async function getPassagers(req,res){
-    const { page, name } = req.query;
+    const { page } = req.query;
     try{
-        // implementar service
-        // implementar repository
         const passagers = await passagersRepositories.getPassagers(page);
         res.status(httpStatus.OK).send(passagers);
     }
